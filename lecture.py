72166fce4eefed31e0a8fca660a0b8db56f64057
rrhoades10 <rrhoades10@yahoo.com>
2024-03-21 17:11:39 -0500

    python strings

---
+++ lecture.py
@@ -0,0 +1,52 @@
+# Adding Special Characters to our strings
+# backslash \ "escapes characters"
+print("Hello \nthere!") #\n create a new line within the string
+print("Hello\tThere!") # \t tabs over between chracters in string
+print("This is a backslash: \\")
+print("This is a really long sentence about nothing\
+      i am excited to play video games this evening and sleep\
+      ")
+print("I called the doctor yesterday and he told me \"You need to stretch more\"")
+print('My son\'s teeth are coming in and he is not sleeping')
+
+name = "jim-bob"
+
+spanish_string = "Quiero ir al parque mañana"
+
+#String Concatenation
+greeting = "Hello There"
+name = "General Kenobi"
+print(greeting + ", " + name + "!")
+greeting_general = greeting + ", " + name + "!"
+print(greeting_general)
+
+# # concatenating user_input
+# first_name = input("Please enter your first name: ")
+# last_name = input("Please enter your last name: ")
+# full_name = first_name + " " + last_name
+# print("Hello!", full_name)
+name = "Obi Wan"
+name2 = "Anakin"
+name3 = "Yoda"
+print("Hello", "how", "are", "you", "today?")
+print(name, name2, name3)
+
+greeting = "Hello There"
+name = "General Kenobi"
+
+age = 32
+print(name + " is " + str(age) + " years old")
+
+
+# Customize your printed messages
+print("apple", "cherry", "banana", sep="-")
+
+print("This is just the beginning....", end="")
+print(" and this is the end...", end="")
+print("and here is some more stuff")
+
+print("chocolate chip", "moose tracks", "rocky road", sep="***", end="...yum!")
+
+
+
+
